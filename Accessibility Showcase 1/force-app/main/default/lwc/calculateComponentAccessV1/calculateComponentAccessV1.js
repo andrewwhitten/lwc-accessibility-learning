8464461c2a5c009be29a20e5d827d1b6250cba84
andrewwhitten <41508645+andrewwhitten@users.noreply.github.com>
2024-04-20 08:51:02 +1000

    First commit

---
+++ Accessibility Showcase 1/force-app/main/default/lwc/calculateComponentAccessV1/calculateComponentAccessV1.js
@@ -0,0 +1,26 @@
+import { LightningElement } from 'lwc';
+
+import CALC_ICON from "@salesforce/resourceUrl/Calc_Icon";
+
+export default class CalculateComponentAccessV1 extends LightningElement {
+
+    calculatorIconUrl = CALC_ICON + ".jpg";
+
+    valueFirst = 0;
+    valueSecond = 0;
+    calculationResultLabel;
+
+    handleFirstInputChange(event) {
+
+        this.valueFirst = event.target.value;
+    }
+
+    handleSecondInputChange(event) {
+
+        this.valueSecond = event.target.value;
+    }
+
+    handleCalculateClick(event) {
+        this.calculationResultLabel = this.valueFirst * this.valueSecond;
+    }
+}
